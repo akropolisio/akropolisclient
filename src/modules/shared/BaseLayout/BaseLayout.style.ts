572b25195b20b3071c3b37dfd6d503afddbd848d
Дмитрий Хурсевич <varimartas@yandex.ru>
2019-01-07 14:50:09 +0700

    fix header scroll at mobile #53

--- src/modules/shared/BaseLayout/BaseLayout.style.ts
+++ src/modules/shared/BaseLayout/BaseLayout.style.ts
@@ -5,7 +5,12 @@ const styles = (theme: Theme) => ({
   content: rule({
     maxWidth: theme.extra.sizes.page.maxWidth,
     padding: theme.spacing.unit * 3,
-    [theme.breakpoints.up('md')]: {
+    paddingTop: `calc(${theme.spacing.unit}px + ${theme.extra.sizes.header.minHeightMobile})`,
+
+    [theme.extra.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing.unit * 3,
+    },
+    [theme.extra.breakpoints.up('md')]: {
       margin: '0 auto',
     },
   }),
